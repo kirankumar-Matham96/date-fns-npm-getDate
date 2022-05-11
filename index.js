216a863be444ea9f7182646ed9cb96b515b16614
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2022-05-11 15:52:53 +0000

    modified: function to function expression

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const addDays = require("date-fns/addDays");
-function getDate(days) {
+// function getDate(days) {
+module.exports = function (days) {
   return addDays(new Date(2020, 07, 22), days);
-}
+};
 
-module.exports = getDate;
+// module.exports = getDate;
