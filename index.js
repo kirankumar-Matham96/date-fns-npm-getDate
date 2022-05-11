2553eea64ad1b7fba45273c2bedd9a601e917892
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2022-05-11 10:28:18 +0000

    modified input for the function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const addDays = require("date-fns/addDays");
 function getDate(days) {
-  return addDays(new Date(22, 08, 2020), days);
+  return addDays(new Date(2020, 07, 22), days);
 }
 
 module.exports = getDate;
